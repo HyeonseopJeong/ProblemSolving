78e504bcca3c25c0eae7adf0d8bda62c9f4936b5
HyeonseopJeong <gustjq1101@naver.com>
2020-12-29 11:09:41 +0900

    겨울방학 1주차 -3(완)

---
+++ 2020winter-week1/boj15552.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc; cin >> tc;
+
+    while(tc--) {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+
+
+    return 0;
+}
---
+++ 2020winter-week1/boj2446.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;  cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i; j++)
+            cout << ' ';
+        for(int j = 0; j < (n - i) * 2 - 1; j++)
+            cout << '*';
+        cout << '\n';
+    }
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 2; j++)
+            cout << ' ';
+        for(int j = 0; j < i * 2 + 3; j++)
+            cout << '*';
+        cout << '\n';
+    }
+    return 0;
+}
